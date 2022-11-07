b401c49b20134eb28be3870bded03895462fe08d
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-11-07 18:30:17 +0700

    String records

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,5 +1,5 @@
 use ferrite::{
-    entry_point, AnyVariable, Context, Downcast, ReadArrayVariable, ReadVariable, Registry,
+    entry_point, AnyVariable, Context, Downcast, ReadArrayVariable, ReadVariable,
     WriteArrayVariable, WriteVariable,
 };
 use futures::{
@@ -20,30 +20,35 @@ fn app_main(mut ctx: Context) {
     block_on(async_main(exec, ctx));
 }
 
-fn take_from_registry<V>(reg: &mut Registry, name: &str) -> Option<V>
+fn take_var<V>(ctx: &mut Context, name: &str) -> V
 where
     AnyVariable: Downcast<V>,
 {
-    reg.remove(name)?.downcast()
+    log::debug!("take: {}", name);
+    let var = ctx
+        .registry
+        .remove(name)
+        .expect(&format!("No such name: {}", name));
+    let dir = var.direction();
+    let dty = var.data_type();
+    var.downcast()
+        .expect(&format!("Bad type, {:?}: {:?} expected", dir, dty))
 }
 
 async fn async_main(exec: ThreadPool, mut ctx: Context) {
     log::info!("IOC started");
 
-    let mut ai: WriteVariable<i32> = take_from_registry(&mut ctx.registry, "example:ai").unwrap();
-    let mut ao: ReadVariable<i32> = take_from_registry(&mut ctx.registry, "example:ao").unwrap();
-    let mut aai: WriteArrayVariable<i32> =
-        take_from_registry(&mut ctx.registry, "example:aai").unwrap();
-    let mut aao: ReadArrayVariable<i32> =
-        take_from_registry(&mut ctx.registry, "example:aao").unwrap();
-    let mut waveform: WriteArrayVariable<i32> =
-        take_from_registry(&mut ctx.registry, "example:waveform").unwrap();
-    let mut bi: WriteVariable<u32> = take_from_registry(&mut ctx.registry, "example:bi").unwrap();
-    let mut bo: ReadVariable<u32> = take_from_registry(&mut ctx.registry, "example:bo").unwrap();
-    let mut mbbi_direct: WriteVariable<u32> =
-        take_from_registry(&mut ctx.registry, "example:mbbiDirect").unwrap();
-    let mut mbbo_direct: ReadVariable<u32> =
-        take_from_registry(&mut ctx.registry, "example:mbboDirect").unwrap();
+    let mut ai: WriteVariable<f64> = take_var(&mut ctx, "example:ai");
+    let mut ao: ReadVariable<f64> = take_var(&mut ctx, "example:ao");
+    let mut aai: WriteArrayVariable<i32> = take_var(&mut ctx, "example:aai");
+    let mut aao: ReadArrayVariable<i32> = take_var(&mut ctx, "example:aao");
+    let mut waveform: WriteArrayVariable<i32> = take_var(&mut ctx, "example:waveform");
+    let mut bi: WriteVariable<u16> = take_var(&mut ctx, "example:bi");
+    let mut bo: ReadVariable<u16> = take_var(&mut ctx, "example:bo");
+    let mut mbbi_direct: WriteVariable<u32> = take_var(&mut ctx, "example:mbbiDirect");
+    let mut mbbo_direct: ReadVariable<u32> = take_var(&mut ctx, "example:mbboDirect");
+    let mut stringin: WriteArrayVariable<u8> = take_var(&mut ctx, "example:stringin");
+    let mut stringout: ReadArrayVariable<u8> = take_var(&mut ctx, "example:stringout");
 
     assert!(ctx.registry.is_empty());
 
@@ -77,6 +82,19 @@ async fn async_main(exec: ThreadPool, mut ctx: Context) {
             let value = mbbo_direct.read().await;
             log::info!("mbbo_direct -> mbbi_direct: {:016b}", value);
             mbbi_direct.write(value).await;
+        }
+    });
+    exec.spawn_ok(async move {
+        assert!(stringout.max_len() <= stringin.max_len());
+        let mut buffer = vec![0; stringout.max_len()];
+        loop {
+            let len = stringout.read_to_slice(&mut buffer).await.unwrap();
+            let value = &buffer[..len];
+            log::info!(
+                "stringout -> stringin: '{}'",
+                String::from_utf8_lossy(value)
+            );
+            stringin.write_from_slice(value).await;
         }
     });
 
