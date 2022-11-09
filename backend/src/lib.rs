b9145d544ea49445a85af55633c63c82e500605b
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-11-09 09:49:51 +0700

    Test writeback

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -35,22 +35,22 @@ async fn async_main(exec: ThreadPool, mut ctx: Context) {
     log::info!("IOC started");
 
     let mut ai: Variable<f64, false, true, true> = take_var(&mut ctx, "example:ai");
-    let mut ao: Variable<f64, true, false, false> = take_var(&mut ctx, "example:ao");
+    let mut ao: Variable<f64, true, true, true> = take_var(&mut ctx, "example:ao");
     //let mut aai: WriteArrayVariable<i32> = take_var(&mut ctx, "example:aai");
     //let mut aao: ReadArrayVariable<i32> = take_var(&mut ctx, "example:aao");
     //let mut waveform: WriteArrayVariable<i32> = take_var(&mut ctx, "example:waveform");
     let mut bi: Variable<u16, false, true, true> = take_var(&mut ctx, "example:bi");
-    let mut bo: Variable<u16, true, false, false> = take_var(&mut ctx, "example:bo");
+    let mut bo: Variable<u16, true, true, true> = take_var(&mut ctx, "example:bo");
     let mut mbbi_direct: Variable<u32, false, true, true> =
         take_var(&mut ctx, "example:mbbiDirect");
-    let mut mbbo_direct: Variable<u32, true, false, false> =
-        take_var(&mut ctx, "example:mbboDirect");
+    let mut mbbo_direct: Variable<u32, true, true, true> = take_var(&mut ctx, "example:mbboDirect");
     //let mut stringin: WriteArrayVariable<u8> = take_var(&mut ctx, "example:stringin");
     //let mut stringout: ReadArrayVariable<u8> = take_var(&mut ctx, "example:stringout");
 
     assert!(ctx.registry.is_empty());
 
     exec.spawn_ok(async move {
+        ao.request().await.write(std::f64::consts::PI).await;
         loop {
             let value = ao.wait().await.read().await;
             log::info!("ao -> ai: {}", value);
@@ -71,6 +71,7 @@ async fn async_main(exec: ThreadPool, mut ctx: Context) {
     });
     */
     exec.spawn_ok(async move {
+        bo.request().await.write(1).await;
         loop {
             let value = bo.wait().await.read().await;
             log::info!("bo -> bi: {}", value != 0);
@@ -78,6 +79,7 @@ async fn async_main(exec: ThreadPool, mut ctx: Context) {
         }
     });
     exec.spawn_ok(async move {
+        mbbo_direct.request().await.write(0xffff).await;
         loop {
             let value = mbbo_direct.wait().await.read().await;
             log::info!("mbbo_direct -> mbbi_direct: {:016b}", value);
